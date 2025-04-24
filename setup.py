75b10e3e818f7fdc2bc12aa0e1b54e25244694e9
Himanshu-4 <jangragi123@gmail.com>
2025-04-24 19:37:18 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='hci',
+    version='0.0.8',
+    description='Library for creating and parsing HCI packets.',
+    url='https://github.com/Himanshu-4/hci-tool',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords=['HCI', 'protocol','encode', 'decode',
+              'ble', 'bluetooth low energy'],
+    packages=find_packages(exclude=['struct', 'docs', 'os']),
+    author='Himanshu',
+    author_email='himanshuj0304@gmail.com',
+
+)
