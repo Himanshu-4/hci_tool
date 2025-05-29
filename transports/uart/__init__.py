ce41826c23659ca9a280f7b3e7293914bf82b9be
Himanshu <himanshuj0304@gmail.com>
2025-05-29 23:26:48 +0530

    Add comments to clarify module imports in UART and USB transport layers

--- transports/uart/__init__.py
+++ transports/uart/__init__.py
@@ -4,4 +4,5 @@ UART transport layer package.
 
 from .uart import *
 
+#import all modules in the package
 __all__ = []
--- transports/usb/__init__.py
+++ transports/usb/__init__.py
@@ -4,4 +4,5 @@ USB transport layer package.
 
 from .usb import *
 
+# Import all modules in the package
 __all__ = []
